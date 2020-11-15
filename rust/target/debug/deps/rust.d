1f59661f84583a91dd2208619b432b12c6bb7b8f
David Tennander <david.tennander@gmail.com>
2020-11-15 18:42:29 +0100

    Removes rust/target/ directory.

--- rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-/Users/aiman/code/hobby/mandelbrot/rust/target/debug/deps/rust: src/main.rs
-
-/Users/aiman/code/hobby/mandelbrot/rust/target/debug/deps/rust.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/aiman/code/hobby/mandelbrot/rust/target/debug/rust: /Users/aiman/code/hobby/mandelbrot/rust/src/main.rs
